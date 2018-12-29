8cd2b73dd10f7fe3a33c18f2dae06391e2a71af3
Konisko Bandyopadhyay <30558121+Jakoban2411@users.noreply.github.com>
2018-12-29 12:57:50 +0000

    AI movement fixed!

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour {
+    AIMoveDecision WaypointManager;
+    public float MovementSpeed=2;
+    float movementthisframe;
+    bool moving;
+    int index0;
+    Vector2 MoveToPosition;
+    // Use this for initialization
+    void Start () {
+        WaypointManager = FindObjectOfType<AIMoveDecision>();
+        moving = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(moving==false)
+        {
+            index0 = UnityEngine.Random.Range(0,WaypointManager.Waypoints.Count-1);
+            StartCoroutine(RandomMove());
+             MoveToPosition = WaypointManager.Waypoints[index0].transform.position;
+        }
+        if(moving==true)
+        {
+            movementthisframe = MovementSpeed * Time.deltaTime;
+            transform.position = Vector2.MoveTowards(transform.position, MoveToPosition, movementthisframe);
+        }
+	}
+    IEnumerator RandomMove()
+    {
+        moving = true;
+        yield return new WaitForSeconds(2);
+        moving = false;
+    }
+    private void OnCollisionStay(Collision collision)
+    {
+        index0 = UnityEngine.Random.Range(0, WaypointManager.Waypoints.Count - 1);
+        MoveToPosition = WaypointManager.Waypoints[index0].transform.position;
+        moving = true;
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        index0 = UnityEngine.Random.Range(0, WaypointManager.Waypoints.Count - 1);
+        MoveToPosition = WaypointManager.Waypoints[index0].transform.position;
+        moving = true;
+    }
+}
---
+++ Assets/Scripts/WaypointProperties.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaypointProperties : MonoBehaviour {
+    //bool Occupied;
+    [SerializeField] AIMoveDecision WaypointManager;
+	// Use this for initialization
+	void Start () {
+        WaypointManager.Waypoints.Add(this);
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag=="Enemy")
+        {
+            //Occupied = true;
+           WaypointManager.Waypoints.Remove(this);
+           }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+       WaypointManager.Waypoints.Add(this);
+        Debug.Log(" List Count:" + WaypointManager.Waypoints.Count);
+    }
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
