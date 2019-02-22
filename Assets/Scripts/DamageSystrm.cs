553ece8af2959da0abe526f08652128d09eaefcb
Konisko Bandyopadhyay <30558121+Jakoban2411@users.noreply.github.com>
2019-02-22 11:09:57 +0000

    Damage
    Damage Effect Added

--- Assets/Scripts/DamageSystrm.cs
+++ Assets/Scripts/DamageSystrm.cs
@@ -5,19 +5,26 @@ using UnityEngine;
 public class DamageSystrm : MonoBehaviour
 {
     [SerializeField] float CollisionDamage;
-    public float MaxHealth;
-    [HideInInspector] public float Health;
+    public float MaxHealth,Health;
+    //[HideInInspector] public float Health;
     public GameObject[] Pickup;
     GameObject Healthsprite;
     int index;
+    Vector3 Maxscale;
+    bool isPlayer;
     GameObject PickupSprite;
+    [SerializeField]GameObject DamageEffect;
+    Animator Effect;
     // Use this for initialization
     void Start()
     {
         Health = MaxHealth;
         if (gameObject.tag == "Player")
         {
+            isPlayer = true;
             Healthsprite = gameObject.GetComponent<PlayerMovement>().Health;
+            Maxscale = Healthsprite.transform.localScale;
+            Effect=DamageEffect.GetComponent<Animator>();
         }
         index = UnityEngine.Random.Range(0, Pickup.Length);
     }
@@ -30,9 +37,11 @@ public class DamageSystrm : MonoBehaviour
     public void Damage(float HitPoints)
     {
         Health -= HitPoints;
+
         if (Health <= 0)
         {
-            if (Random.value >= Pickup[index].GetComponent<Pickup>().DropProbability)
+
+            if (isPlayer == false && Random.value >= Pickup[index].GetComponent<Pickup>().DropProbability)
             {
                 Instantiate<GameObject>(Pickup[index], transform.position, Pickup[index].transform.rotation);
             }
@@ -41,8 +50,16 @@ public class DamageSystrm : MonoBehaviour
         }
         else
         {
-            if (Healthsprite)
-                Healthsprite.transform.localScale = Healthsprite.transform.localScale * (Health / MaxHealth);
+            if (isPlayer==true)
+            {
+                if(DamageEffect.activeSelf==false)
+                {
+                    DamageEffect.SetActive(true);
+                }
+                Healthsprite.transform.localScale = Maxscale * (Health / MaxHealth);
+                Effect.Play("DamageEffect");
+                
+            }
         }
     }
     private void OnCollisionEnter2D(Collision2D collision)
